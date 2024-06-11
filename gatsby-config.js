737f8c43010ca3c366b97808fe1f59717b2bcc1e
tonymoceri <78163395+tonymoceri@users.noreply.github.com>
2024-06-11 16:11:19 -0400

    added prettier config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,10 @@
 module.exports = {
   pathPrefix: "/tonymoceri.github.io",
   siteMetadata: {
-    title: `Personal Portfolio`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "Tony Moceri || Data Scientist",
+    description: "My name is Tony Moceri, and I made this site to not only showcase my work, but to also share my thoughts on data, technology, and life!",
+    siteUrl: "https://www.tonymoceri.com",
+    author: "tonymoceri"
   },
   plugins: [],
 }
---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+const options ={
+  trailingComma: "all",
+  tabwidth: 2,
+  useTabs: false,
+  singleQuote: false,
+  arrowParens: "always",
+};
+
+module.exports = options;
