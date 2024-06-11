b2407040a6e2ba7765a2cd37a271f9cbeab4d86d
tonymoceri <78163395+tonymoceri@users.noreply.github.com>
2024-06-11 15:16:17 -0400

    added the about page

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+
+import * as React from 'react'
+
+const AboutPage = () => {
+return (
+<main>
+<h1>About Me</h1>
+<p>Hi there! My name is Tony Moceri, and welcome to my website! Here, you will find projects, work experience, and contact info for
+me.</p>
+</main>
+)
+}
+
+export const Head = () => <title>About Me</title>
+
+export default AboutPage
+
+
