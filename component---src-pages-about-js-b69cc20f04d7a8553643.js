0395288ac85bbd032e77fe8467ceb7e25bc8d155
tonymoceri <78163395+tonymoceri@users.noreply.github.com>
2024-06-11 15:11:39 -0400

    Updates

---
+++ component---src-pages-about-js-b69cc20f04d7a8553643.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpersonal_portfolio=self.webpackChunkpersonal_portfolio||[]).push([[682],{5465:function(e,n,o){o.r(n);var l=o(7294);n.default=()=>l.createElement("main",null,l.createElement("h1",null,"About Me"),l.createElement("p",null,"Hi there! My name is Tony Moceri, and welcome to my website! Here, you will find projects, work experience, and contact info for me."))}}]);
+//# sourceMappingURL=component---src-pages-about-js-b69cc20f04d7a8553643.js.map
---
+++ component---src-pages-index-js-2f0b70762f819324a685.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpersonal_portfolio=self.webpackChunkpersonal_portfolio||[]).push([[678],{6558:function(e,t,l){l.r(t),l.d(t,{Head:function(){return a}});var n=l(7294);const a=()=>n.createElement("title",null,"Home Page");t.default=()=>n.createElement("main",null,n.createElement("h1",null,"Welcome to my Gatsby site!"),n.createElement("p",null,"I'm making this by following the Gatsby Tutorial."))}}]);
+//# sourceMappingURL=component---src-pages-index-js-2f0b70762f819324a685.js.map
