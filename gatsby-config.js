ba4a7f4197b25e5332c68b10c80e071c5cb703af
tonymoceri <78163395+tonymoceri@users.noreply.github.com>
2024-06-11 18:50:57 -0400

    edited the package.json and the config files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,41 @@
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
-  pathPrefix: "/tonymoceri.github.io",
   siteMetadata: {
     title: "Tony Moceri || Data Scientist",
-    description: "My name is Tony Moceri, and I made this site to not only showcase my work, but to also share my thoughts on data, technology, and life!",
+    description:
+      "My mission as a developer is to contribute to applications that empower and motivate lives. Let's change the world together.",
     siteUrl: "https://www.tonymoceri.com",
-    author: "tonymoceri"
+    author: "tonymoceri",
   },
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Tony Moceri`,
+        short_name: `TonyMoceri`,
+        start_url: `/`,
+        background_color: `#337ab7`,
+        theme_color: `#337ab7`,
+        display: `fullscreen`,
+        icon: `src/static/logo/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/images`,
+        name: `images`,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-react-helmet`,
+  ],
+};
