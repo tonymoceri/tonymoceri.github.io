8724e3de5e4f1e595f2d0006cd97b90291d221d3
tonymoceri <78163395+tonymoceri@users.noreply.github.com>
2024-06-11 15:19:44 -0400

    edited about file

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,14 +2,14 @@
 import * as React from 'react'
 
 const AboutPage = () => {
-return (
-<main>
-<h1>About Me</h1>
-<p>Hi there! My name is Tony Moceri, and welcome to my website! Here, you will find projects, work experience, and contact info for
-me.</p>
-</main>
-)
+    return (
+    <main>
+        <h1>About Me</h1>
+        <p>Hi there! My name is Tony Moceri, and welcome to my website! Here, you will find projects, work experience, and contact info for me.</p>
+        </main> 
+    )
 }
+
 
 export const Head = () => <title>About Me</title>
 
