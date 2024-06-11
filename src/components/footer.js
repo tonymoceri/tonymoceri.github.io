c7536bc531159dcfb1ef8f838588e41e4f1db34c
tonymoceri <78163395+tonymoceri@users.noreply.github.com>
2024-06-11 18:26:35 -0400

    added footer files

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => (
+  <footer>
+    <div>
+      Inspired by Maribel Duran <i className="fa fa-heart" aria-hidden="true" />
+      <br />
+      Coded by Tony Moceri
+    </div>
+  </footer>
+);
+
+export default Footer;
