a2a6ab1633cce387c95a624a9d1070dba28c0e82
tonymoceri <78163395+tonymoceri@users.noreply.github.com>
2024-06-11 14:47:43 -0400

    first push of public onfo

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  pathPrefix: "/tonymoceri.github.io",
   siteMetadata: {
     title: `Personal Portfolio`,
     siteUrl: `https://www.yourdomain.tld`,
